66833be36abdebcaab07a5f19894db6c0daa7565
dingtj <dingtj@shanghaitech.edu.cn>
2017-05-27 14:15:32 +0800

    solved big problem

--- src/http.rs
+++ src/http.rs
@@ -2,9 +2,10 @@ extern crate hyper;
 extern crate url;
 use self::hyper::Client;
 use self::hyper::client::{Body, RequestBuilder};
+use self::hyper::client::response::Response;
 use self::hyper::header::{Header, HeaderFormat};
+use self::hyper::error::Error;
 use self::url::Url;
-use std::marker::PhantomData;
 
 
 pub enum Method {
@@ -18,32 +19,24 @@ where B: 'a + Into<Body<'a>>
     pub url: Url,
     pub method: Method,
     pub body: Option<B>,
-    pub client: Client,
-    phantom: PhantomData<&'a B>,
+    pub client: &'a Client,
 }
 
 impl<'a, B: 'a + Into<Body<'a>>> Request<'a, B> {
-    fn download(self) {
-        // let url_str = self.url.as_str();
-        // let client: RequestBuilder;
-        // match self.method {
-        //     ref Get => {
-        //         client = self.client.get(url_str);
-        //     },
-        //     ref Post => {
-        //         client = self.client.post(url_str);
-        //     },
-        // }
-        // let mut client = self.client.get("haha.com");
-    //     if let Some(body) = self.body{
-        // let client = client.body(self.body.unwrap());
-    //     }
-        let cl1 = self.client;
-        let mut cl3 = cl1.get("haha.com");
-        let body2 = self.body.unwrap();
-        // let body3 = body2.into();
-        // {
-            cl3 = cl3.body(body2);
-        // }
+    fn download(self)->Result<Response, Error> {
+        let url_str = self.url.as_str();
+        let mut client: RequestBuilder;
+        match self.method {
+            ref Get => {
+                client = self.client.get(url_str);
+            },
+            ref Post => {
+                client = self.client.post(url_str);
+            },
+        }
+        if let Some(body) = self.body{
+            client = client.body(body);
+        }
+        client.send()
     }
 }
